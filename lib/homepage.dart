8058bd0390df5371d7c61ab38d4dbff3e7ad7328
Jerin <jerinjacob0206@gmail.com>
2022-11-15 16:14:24 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsivedemo/responsive/desktop_body.dart';
+import 'package:responsivedemo/responsive/mobile_body.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var deviceWidth = MediaQuery.of(context).size.width;
+
+    if (deviceWidth < 600) {
+      return const MyMobileBody();
+    } else {
+      return const MyDesktopBody();
+    }
+  }
+}
